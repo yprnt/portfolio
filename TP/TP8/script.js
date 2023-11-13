07ce3d16f476f4e8372917dbcc78d47c69ebe54e
yprnt <145359823+yprnt@users.noreply.github.com>
2023-11-13 16:57:39 +0100

    Update TP8
    Ajout du bonus et de plusieurs exercices

--- TP/TP8/script.js
+++ TP/TP8/script.js
@@ -25,6 +25,27 @@ function connexion() {
     }
 }
 
+function seconnecter3() {
+    var i = 0;
+    do {
+        var id = prompt("Donner votre nom d'utilisateur");
+        var mdp = prompt("Entrez votre mot de passe");
+
+        if (id == "admin" && mdp == "admin") {
+            document.write("Bienvenue : " +id);
+            break;
+        }
+        else {
+            alert("Accès refusé")
+        }
+        i +=1;
+    } while(i<3);
+    if (i==3) {
+        alert("Délai dépassé");
+    }
+}
+
+
 function cdc() {
     var chaine = prompt("Donnez un mot");
     document.write(chaine.toUpperCase()+"<br>");
@@ -81,4 +102,52 @@ function affichagetab2() {
         document.write(`<tr><td>${i}</td><td>*</td><td>*</td></tr>`);
     }
     document.write("</table>");
+}
+
+function tab() {
+    window.location.href='tab_dynamique.html'
+}
+
+function ajouterLigne() {
+    var a = document.getElementById("t1").value;
+    var b = document.getElementById("t2").value;
+
+    if(a=="" || b == "") {
+        alert("un ou 2 champs vide");
+    }
+    else if ((Number(b)<10 || Number(b) > 20)) {
+        alert("L'age n'est pas valide");
+    }
+    else {
+        var table = document.getElementById("MyTable");
+        var newRow = table.insertRow(-1);
+        var cell1 = newRow.insertCell(0);
+        var cell2 = newRow.insertCell(1);
+        
+        cell1.innerHTML = document.getElementById("t1").value;
+        cell2.innerHTML = document.getElementById("t2").value;
+    }
+}
+
+
+function bonus() {
+    var affich1 = "";
+    var somme_tot = 0;
+    do {
+        var article = prompt("Votre article");
+        var prix = prompt("Le prix");
+        var quantite = prompt("La quantité");
+        var somme = (Number(prix) * Number(quantite));
+        somme_tot += somme;
+        alert("Vos " + article + " coûteront " + somme + " €");
+        affich1 += ("Article : " + article + "<br> Prix : " + prix + "€" + "<br> Quantité : " + quantite + "<br> Total : " + somme + " € " + "<br><br><br>");
+        alert("Vos articles additionnés coûteront " + somme_tot + " €");
+        var suite = prompt("Voulez-vous continuer (OUI/STOP) ?")
+    }while(suite == "OUI");
+    if (suite == "STOP") {
+        document.write(affich1);
+        document.write("Prix total : " + somme_tot + " €");
+
+    }
+
 }
